7f8a624fcb2ceaa6fc4fbdfec47f5fd584c2eb13
chiddy <chiddy@plsnobully.me>
2020-11-13 07:23:42 +1000

    Revert "remove gen and shared_gen, fix formatting in cmake"
    This reverts commit 84cebe5fbfee7a46d676e8b877a7f74c631bd9c6.

---
+++ shared_gen/cpp/components/sprite_animation_component.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#ifndef DONT_INCLUDE_HALLEY_HPP
+#include <halley.hpp>
+#endif
+
+class SpriteAnimationComponent final : public Halley::Component {
+public:
+	static constexpr int componentIndex{ 3 };
+	static const constexpr char* componentName{ "SpriteAnimation" };
+
+	Halley::AnimationPlayer player{};
+
+	SpriteAnimationComponent() {
+	}
+
+	SpriteAnimationComponent(Halley::AnimationPlayer player)
+		: player(std::move(player))
+	{
+	}
+
+	Halley::ConfigNode serialize(const Halley::ConfigNodeSerializationContext& context) const {
+		using namespace Halley::EntitySerialization;
+		Halley::ConfigNode node = Halley::ConfigNode::MapType();
+		Halley::EntityConfigNodeSerializer<decltype(player)>::serialize(player, Halley::AnimationPlayer{}, context, node, "player", makeMask(Type::Prefab, Type::SaveData));
+		return node;
+	}
+
+	void deserialize(const Halley::ConfigNodeSerializationContext& context, const Halley::ConfigNode& node) {
+		using namespace Halley::EntitySerialization;
+		Halley::EntityConfigNodeSerializer<decltype(player)>::deserialize(player, context, node, "player", makeMask(Type::Prefab, Type::SaveData));
+	}
+
+};
---
+++ shared_gen/cpp/components/sprite_component.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#ifndef DONT_INCLUDE_HALLEY_HPP
+#include <halley.hpp>
+#endif
+
+class SpriteComponent final : public Halley::Component {
+public:
+	static constexpr int componentIndex{ 1 };
+	static const constexpr char* componentName{ "Sprite" };
+
+	Halley::Sprite sprite{};
+	int layer{ 0 };
+	Halley::OptionalLite<int> mask{};
+
+	SpriteComponent() {
+	}
+
+	SpriteComponent(Halley::Sprite sprite, int layer, Halley::OptionalLite<int> mask)
+		: sprite(std::move(sprite))
+		, layer(std::move(layer))
+		, mask(std::move(mask))
+	{
+	}
+
+	Halley::ConfigNode serialize(const Halley::ConfigNodeSerializationContext& context) const {
+		using namespace Halley::EntitySerialization;
+		Halley::ConfigNode node = Halley::ConfigNode::MapType();
+		Halley::EntityConfigNodeSerializer<decltype(sprite)>::serialize(sprite, Halley::Sprite{}, context, node, "sprite", makeMask(Type::Prefab, Type::SaveData));
+		Halley::EntityConfigNodeSerializer<decltype(layer)>::serialize(layer, int{ 0 }, context, node, "layer", makeMask(Type::Prefab, Type::SaveData));
+		Halley::EntityConfigNodeSerializer<decltype(mask)>::serialize(mask, Halley::OptionalLite<int>{}, context, node, "mask", makeMask(Type::Prefab, Type::SaveData));
+		return node;
+	}
+
+	void deserialize(const Halley::ConfigNodeSerializationContext& context, const Halley::ConfigNode& node) {
+		using namespace Halley::EntitySerialization;
+		Halley::EntityConfigNodeSerializer<decltype(sprite)>::deserialize(sprite, context, node, "sprite", makeMask(Type::Prefab, Type::SaveData));
+		Halley::EntityConfigNodeSerializer<decltype(layer)>::deserialize(layer, context, node, "layer", makeMask(Type::Prefab, Type::SaveData));
+		Halley::EntityConfigNodeSerializer<decltype(mask)>::deserialize(mask, context, node, "mask", makeMask(Type::Prefab, Type::SaveData));
+	}
+
+};
