fccff3d8f162a6792bbb4fbaa8c5a9c87d594c6a
chiddy <chiddy@plsnobully.me>
2020-11-22 11:48:23 +1000

    added overload for loaddependencies to use recources&

--- src/engine/core/include/halley/core/graphics/sprite/animation.h
+++ src/engine/core/include/halley/core/graphics/sprite/animation.h
@@ -8,152 +8,153 @@
 
 namespace Halley
 {
-	class Deserializer;
-	class Serializer;
-	class ResourceLoader;
-	class SpriteSheet;
-	class SpriteSheetEntry;
-	class Material;
-	class AnimationDirection;
-	class AnimationImporter;
+    class Deserializer;
+    class Serializer;
+    class ResourceLoader;
+    class SpriteSheet;
+    class SpriteSheetEntry;
+    class Material;
+    class AnimationDirection;
+    class AnimationImporter;
 
-	class AnimationFrame
-	{
-	public:
-		AnimationFrame(int frameNumber, int duration, const String& imageName, const SpriteSheet& sheet, const Vector<AnimationDirection>& directions);
+    class AnimationFrame
+    {
+    public:
+        AnimationFrame( int frameNumber, int duration, const String& imageName, const SpriteSheet& sheet, const Vector< AnimationDirection >& directions );
 
-		const SpriteSheetEntry& getSprite(int dir) const
-		{
-			Expects(dir >= 0 && dir < int(sprites.size()));
-			return *sprites[dir];
-		}
-		int getDuration() const;
+        const SpriteSheetEntry& getSprite( int dir ) const
+        {
+            Expects( dir >= 0 && dir < int( sprites.size() ) );
+            return *sprites[ dir ];
+        }
+        int getDuration() const;
 
-	private:
-		Vector<const SpriteSheetEntry*> sprites;
-		int duration;
-	};
+    private:
+        Vector< const SpriteSheetEntry* > sprites;
+        int duration;
+    };
 
-	class AnimationFrameDefinition
-	{
-	public:
-		AnimationFrameDefinition();
-		AnimationFrameDefinition(int frameNumber, int duration, String imageName);
-		AnimationFrame makeFrame(const SpriteSheet& sheet, const Vector<AnimationDirection>& directions) const;
+    class AnimationFrameDefinition
+    {
+    public:
+        AnimationFrameDefinition();
+        AnimationFrameDefinition( int frameNumber, int duration, String imageName );
+        AnimationFrame makeFrame( const SpriteSheet& sheet, const Vector< AnimationDirection >& directions ) const;
 
-		void serialize(Serializer& s) const;
-		void deserialize(Deserializer& s);
+        void serialize( Serializer& s ) const;
+        void deserialize( Deserializer& s );
 
-	private:
-		String imageName;
-		int frameNumber;
-		int duration;
-	};
+    private:
+        String imageName;
+        int frameNumber;
+        int duration;
+    };
 
-	class AnimationSequence
-	{
-		friend class Animation;
+    class AnimationSequence
+    {
+        friend class Animation;
 
-	public:
-		AnimationSequence();
-		AnimationSequence(String name, bool loop, bool noFlip);
+    public:
+        AnimationSequence();
+        AnimationSequence( String name, bool loop, bool noFlip );
 
-		size_t numFrames() const { return frames.size(); }
-		size_t numFrameDefinitions() const { return frameDefinitions.size(); }
-		const AnimationFrame& getFrame(size_t n) const
-		{
-			Expects(n < frames.size());
-			return frames[n];
-		}
-		const String& getName() const { return name; }
-		bool isLooping() const { return loop; }
-		bool isNoFlip() const { return noFlip; }
+        size_t numFrames() const { return frames.size(); }
+        size_t numFrameDefinitions() const { return frameDefinitions.size(); }
+        const AnimationFrame& getFrame( size_t n ) const
+        {
+            Expects( n < frames.size() );
+            return frames[ n ];
+        }
+        const String& getName() const { return name; }
+        bool isLooping() const { return loop; }
+        bool isNoFlip() const { return noFlip; }
 
-		void serialize(Serializer& s) const;
-		void deserialize(Deserializer& s);
+        void serialize( Serializer& s ) const;
+        void deserialize( Deserializer& s );
 
-		void addFrame(const AnimationFrameDefinition& animationFrameDefinition);
+        void addFrame( const AnimationFrameDefinition& animationFrameDefinition );
 
-		Rect4i getBounds() const;
+        Rect4i getBounds() const;
 
-	private:
-		Vector<AnimationFrame> frames;
-		Vector<AnimationFrameDefinition> frameDefinitions;
-		String name;
-		bool loop = false;
-		bool noFlip = false;
-	};
+    private:
+        Vector< AnimationFrame > frames;
+        Vector< AnimationFrameDefinition > frameDefinitions;
+        String name;
+        bool loop = false;
+        bool noFlip = false;
+    };
 
-	class AnimationDirection
-	{
-		friend class Animation;
+    class AnimationDirection
+    {
+        friend class Animation;
 
-	public:
-		AnimationDirection();
-		AnimationDirection(String name, String fileName, bool flip, int id);
+    public:
+        AnimationDirection();
+        AnimationDirection( String name, String fileName, bool flip, int id );
 
-		String getName() const { return name; }
-		String getFileName() const { return fileName; }
-		bool shouldFlip() const { return flip; }
-		int getId() const { return id; }
+        String getName() const { return name; }
+        String getFileName() const { return fileName; }
+        bool shouldFlip() const { return flip; }
+        int getId() const { return id; }
 
-		bool needsToProcessFrameName(const String& baseName) const;
-		String getFrameName(int frameNumber, String baseName) const;
+        bool needsToProcessFrameName( const String& baseName ) const;
+        String getFrameName( int frameNumber, String baseName ) const;
 
-		void serialize(Serializer& s) const;
-		void deserialize(Deserializer& s);
+        void serialize( Serializer& s ) const;
+        void deserialize( Deserializer& s );
 
-	private:
-		String name;
-		String fileName;
-		int id;
-		bool flip;
-	};
-	
-	class Animation final : public Resource
-	{
-		friend class AnimationImporter;
+    private:
+        String name;
+        String fileName;
+        int id;
+        bool flip;
+    };
 
-	public:
-		Animation();
+    class Animation final : public Resource
+    {
+        friend class AnimationImporter;
 
-		static std::unique_ptr<Animation> loadResource(ResourceLoader& loader);
-		constexpr static AssetType getAssetType() { return AssetType::Animation; }
-		void reload(Resource&& resource) override;
+    public:
+        Animation();
 
-		const String& getName() const { return name; }
-		const SpriteSheet& getSpriteSheet() const { return *spriteSheet; }
-		std::shared_ptr<Material> getMaterial() const { return material; }
+        static std::unique_ptr< Animation > loadResource( ResourceLoader& loader );
+        constexpr static AssetType getAssetType() { return AssetType::Animation; }
+        void reload( Resource&& resource ) override;
 
-		const AnimationSequence& getSequence(const String& name) const;
-		const AnimationDirection& getDirection(const String& name) const;
-		const AnimationDirection& getDirection(int id) const;
-		std::vector<String> getSequenceNames() const;
-		std::vector<String> getDirectionNames() const;
-		
-		Vector2i getPivot() const;
-		Rect4i getBounds() const;
+        const String& getName() const { return name; }
+        const SpriteSheet& getSpriteSheet() const { return *spriteSheet; }
+        std::shared_ptr< Material > getMaterial() const { return material; }
 
-		bool hasSequence(const String& name) const;
+        const AnimationSequence& getSequence( const String& name ) const;
+        const AnimationDirection& getDirection( const String& name ) const;
+        const AnimationDirection& getDirection( int id ) const;
+        std::vector< String > getSequenceNames() const;
+        std::vector< String > getDirectionNames() const;
 
-		void serialize(Serializer& s) const;
-		void deserialize(Deserializer& s);
-		void loadDependencies(ResourceLoader& loader);
+        Vector2i getPivot() const;
+        Rect4i getBounds() const;
 
-		void setName(const String& name);
-		void setMaterialName(const String& name);
-		void setSpriteSheetName(const String& name);
-		void addSequence(const AnimationSequence& sequence);
-		void addDirection(const AnimationDirection& direction);
+        bool hasSequence( const String& name ) const;
 
-	private:
-		String name;
-		String spriteSheetName;
-		String materialName;
-		Vector<AnimationSequence> sequences;
-		Vector<AnimationDirection> directions;
+        void serialize( Serializer& s ) const;
+        void deserialize( Deserializer& s );
+        void loadDependencies( ResourceLoader& loader );
+        void loadDependencies( Resources& res );
 
-		std::shared_ptr<const SpriteSheet> spriteSheet;
-		std::shared_ptr<Material> material;
-	};
-}
+        void setName( const String& name );
+        void setMaterialName( const String& name );
+        void setSpriteSheetName( const String& name );
+        void addSequence( const AnimationSequence& sequence );
+        void addDirection( const AnimationDirection& direction );
+
+    private:
+        String name;
+        String spriteSheetName;
+        String materialName;
+        Vector< AnimationSequence > sequences;
+        Vector< AnimationDirection > directions;
+
+        std::shared_ptr< const SpriteSheet > spriteSheet;
+        std::shared_ptr< Material > material;
+    };
+} // namespace Halley
--- src/engine/core/src/graphics/sprite/animation.cpp
+++ src/engine/core/src/graphics/sprite/animation.cpp
@@ -11,310 +11,341 @@
 
 using namespace Halley;
 
-AnimationFrame::AnimationFrame(int frameNumber, int duration, const String& imageName, const SpriteSheet& sheet, const Vector<AnimationDirection>& directions)
-	: duration(duration)
+AnimationFrame::AnimationFrame( int frameNumber, int duration, const String& imageName, const SpriteSheet& sheet, const Vector< AnimationDirection >& directions ) :
+    duration( duration )
 {
-	const size_t n = directions.size();
-	sprites.resize(n);
-	for (size_t i = 0; i < n; i++) {
-		sprites[i] = &sheet.getSprite(directions[i].needsToProcessFrameName(imageName) ? directions[i].getFrameName(frameNumber, imageName) : imageName);
-	}
+    const size_t n = directions.size();
+    sprites.resize( n );
+    for ( size_t i = 0; i < n; i++ )
+    {
+        sprites[ i ] = &sheet.getSprite( directions[ i ].needsToProcessFrameName( imageName ) ? directions[ i ].getFrameName( frameNumber, imageName ) : imageName );
+    }
 }
 
 int AnimationFrame::getDuration() const
 {
-	return duration;
+    return duration;
 }
 
-AnimationFrameDefinition::AnimationFrameDefinition()
-	: frameNumber(-1)
-{}
+AnimationFrameDefinition::AnimationFrameDefinition() :
+    frameNumber( -1 )
+{ }
 
-AnimationFrameDefinition::AnimationFrameDefinition(int frameNumber, int duration, String imageName)
-	: imageName(std::move(imageName))
-	, frameNumber(frameNumber)
-	, duration(duration)
+AnimationFrameDefinition::AnimationFrameDefinition( int frameNumber, int duration, String imageName ) :
+    imageName( std::move( imageName ) ), frameNumber( frameNumber ), duration( duration )
 {
 }
 
-AnimationFrame AnimationFrameDefinition::makeFrame(const SpriteSheet& sheet, const Vector<AnimationDirection>& directions) const
+AnimationFrame AnimationFrameDefinition::makeFrame( const SpriteSheet& sheet, const Vector< AnimationDirection >& directions ) const
 {
-	return AnimationFrame(frameNumber, duration, imageName, sheet, directions);
+    return AnimationFrame( frameNumber, duration, imageName, sheet, directions );
 }
 
-void AnimationFrameDefinition::serialize(Serializer& s) const
+void AnimationFrameDefinition::serialize( Serializer& s ) const
 {
-	s << imageName;
-	s << frameNumber;
-	s << duration;
+    s << imageName;
+    s << frameNumber;
+    s << duration;
 }
 
-void AnimationFrameDefinition::deserialize(Deserializer& s)
+void AnimationFrameDefinition::deserialize( Deserializer& s )
 {
-	s >> imageName;
-	s >> frameNumber;
-	s >> duration;
+    s >> imageName;
+    s >> frameNumber;
+    s >> duration;
 }
 
-AnimationSequence::AnimationSequence() {}
+AnimationSequence::AnimationSequence() { }
 
-AnimationSequence::AnimationSequence(String name, bool loop, bool noFlip)
-	: name(std::move(name))
-	, loop(loop)
-	, noFlip(noFlip)
-{}
+AnimationSequence::AnimationSequence( String name, bool loop, bool noFlip ) :
+    name( std::move( name ) ), loop( loop ), noFlip( noFlip )
+{ }
 
-void AnimationSequence::serialize(Serializer& s) const
+void AnimationSequence::serialize( Serializer& s ) const
 {
-	s << frameDefinitions;
-	s << name;
-	s << loop;
-	s << noFlip;
+    s << frameDefinitions;
+    s << name;
+    s << loop;
+    s << noFlip;
 }
 
-void AnimationSequence::deserialize(Deserializer& s)
+void AnimationSequence::deserialize( Deserializer& s )
 {
-	s >> frameDefinitions;
-	s >> name;
-	s >> loop;
-	s >> noFlip;
+    s >> frameDefinitions;
+    s >> name;
+    s >> loop;
+    s >> noFlip;
 }
 
-void AnimationSequence::addFrame(const AnimationFrameDefinition& animationFrameDefinition)
+void AnimationSequence::addFrame( const AnimationFrameDefinition& animationFrameDefinition )
 {
-	frameDefinitions.push_back(animationFrameDefinition);
+    frameDefinitions.push_back( animationFrameDefinition );
 }
 
 Rect4i AnimationSequence::getBounds() const
 {
-	Vector2i topLeft(99999, 99999);
-	Vector2i bottomRight;
-	for (auto& frame: frames) {
-		auto& sprite = frame.getSprite(0);
-		if (sprite.size.x >= 0.1f && sprite.size.y > 0.0f) {
-			auto offset = Vector2i(Vector2f(sprite.pivot * sprite.size).round());
-			const Vector2i tl = -offset;
-			const Vector2i br = tl + Vector2i(sprite.size);
+    Vector2i topLeft( 99999, 99999 );
+    Vector2i bottomRight;
+    for ( auto& frame : frames )
+    {
+        auto& sprite = frame.getSprite( 0 );
+        if ( sprite.size.x >= 0.1f && sprite.size.y > 0.0f )
+        {
+            auto offset = Vector2i( Vector2f( sprite.pivot * sprite.size ).round() );
+            const Vector2i tl = -offset;
+            const Vector2i br = tl + Vector2i( sprite.size );
 
-			topLeft = Vector2i::min(topLeft, tl);
-			bottomRight = Vector2i::max(bottomRight, br);
-		}
-	}
+            topLeft = Vector2i::min( topLeft, tl );
+            bottomRight = Vector2i::max( bottomRight, br );
+        }
+    }
 
-	return Rect4i(topLeft, bottomRight);
+    return Rect4i( topLeft, bottomRight );
 }
 
-AnimationDirection::AnimationDirection()
-	: id(-1)
-	, flip(false)
-{}
+AnimationDirection::AnimationDirection() :
+    id( -1 ), flip( false )
+{ }
 
-AnimationDirection::AnimationDirection(String name, String fileName, bool flip, int id)
-	: name(std::move(name))
-	, fileName(std::move(fileName))
-	, id(id)
-	, flip(flip)
+AnimationDirection::AnimationDirection( String name, String fileName, bool flip, int id ) :
+    name( std::move( name ) ), fileName( std::move( fileName ) ), id( id ), flip( flip )
 {
 }
 
-bool AnimationDirection::needsToProcessFrameName(const String& baseName) const
+bool AnimationDirection::needsToProcessFrameName( const String& baseName ) const
 {
-	return baseName.find("%f") != std::string::npos || baseName.find("%dir%") != std::string::npos;
+    return baseName.find( "%f" ) != std::string::npos || baseName.find( "%dir%" ) != std::string::npos;
 }
 
-String AnimationDirection::getFrameName(int frameNumber, String baseName) const
+String AnimationDirection::getFrameName( int frameNumber, String baseName ) const
 {
-	try {
-		baseName = baseName.replaceAll("%dir%", fileName);
+    try
+    {
+        baseName = baseName.replaceAll( "%dir%", fileName );
 
-		size_t frameToken = baseName.find("%f");
-		if (frameToken != std::string::npos) {
-			size_t endToken = baseName.find('%', frameToken + 1);
-			if (endToken != std::string::npos) {
-				int minWidth = 0;
-				if (baseName[frameToken + 2] == ':' && baseName[frameToken + 3] == '0') {
-					minWidth = baseName.subToInteger(frameToken + 4, endToken);
-				}
+        size_t frameToken = baseName.find( "%f" );
+        if ( frameToken != std::string::npos )
+        {
+            size_t endToken = baseName.find( '%', frameToken + 1 );
+            if ( endToken != std::string::npos )
+            {
+                int minWidth = 0;
+                if ( baseName[ frameToken + 2 ] == ':' && baseName[ frameToken + 3 ] == '0' )
+                {
+                    minWidth = baseName.subToInteger( frameToken + 4, endToken );
+                }
 
-				std::stringstream ss;
-				ss << std::setfill('0') << std::setw(minWidth) << frameNumber;
-				baseName = baseName.left(frameToken) + ss.str() + baseName.mid(endToken + 1);
-			}
-		}
+                std::stringstream ss;
+                ss << std::setfill( '0' ) << std::setw( minWidth ) << frameNumber;
+                baseName = baseName.left( frameToken ) + ss.str() + baseName.mid( endToken + 1 );
+            }
+        }
 
-		return baseName;
-	} catch (...) {
-		throw Exception("Invalid frame name: " + baseName, HalleyExceptions::Graphics);
-	}
+        return baseName;
+    } catch ( ... )
+    {
+        throw Exception( "Invalid frame name: " + baseName, HalleyExceptions::Graphics );
+    }
 }
 
-void AnimationDirection::serialize(Serializer& s) const
+void AnimationDirection::serialize( Serializer& s ) const
 {
-	s << name;
-	s << fileName;
-	s << id;
-	s << flip;
+    s << name;
+    s << fileName;
+    s << id;
+    s << flip;
 }
 
-void AnimationDirection::deserialize(Deserializer& s)
+void AnimationDirection::deserialize( Deserializer& s )
 {
-	s >> name;
-	s >> fileName;
-	s >> id;
-	s >> flip;
+    s >> name;
+    s >> fileName;
+    s >> id;
+    s >> flip;
 }
 
-Animation::Animation() 
-{}
+Animation::Animation()
+{ }
 
-std::unique_ptr<Animation> Animation::loadResource(ResourceLoader& loader)
+std::unique_ptr< Animation > Animation::loadResource( ResourceLoader& loader )
 {
-	auto sData = loader.getStatic(false);
-	if (!sData) {
-		return {};
-	}
-	
-	auto result = std::make_unique<Animation>();
-	Deserializer s(sData->getSpan());
-	s >> *result;
-	result->loadDependencies(loader);
-	return result;
+    auto sData = loader.getStatic( false );
+    if ( !sData )
+    {
+        return {};
+    }
+
+    auto result = std::make_unique< Animation >();
+    Deserializer s( sData->getSpan() );
+    s >> *result;
+    result->loadDependencies( loader );
+    return result;
 }
 
-void Animation::reload(Resource&& resource)
+void Animation::reload( Resource&& resource )
 {
-	*this = std::move(dynamic_cast<Animation&>(resource));
+    *this = std::move( dynamic_cast< Animation& >( resource ) );
 }
 
-void Animation::loadDependencies(ResourceLoader& loader)
+void Animation::loadDependencies( ResourceLoader& loader )
 {
-	spriteSheet = loader.getResources().get<SpriteSheet>(spriteSheetName);
+    spriteSheet = loader.getResources().get< SpriteSheet >( spriteSheetName );
 
-	auto matDef = loader.getResources().get<MaterialDefinition>(materialName);
-	material = std::make_shared<Material>(matDef);
-	material->set("tex0", spriteSheet->getTexture());
+    auto matDef = loader.getResources().get< MaterialDefinition >( materialName );
+    material = std::make_shared< Material >( matDef );
+    material->set( "tex0", spriteSheet->getTexture() );
 
-	for (auto& s: sequences) {
-		for (auto& f : s.frameDefinitions) {
-			s.frames.emplace_back(f.makeFrame(*spriteSheet, directions));
-		}
-	}
+    for ( auto& s : sequences )
+    {
+        for ( auto& f : s.frameDefinitions )
+        {
+            s.frames.emplace_back( f.makeFrame( *spriteSheet, directions ) );
+        }
+    }
 }
 
-void Animation::setName(const String& n)
+void Animation::loadDependencies( Resources& res )
 {
-	name = n;
+    spriteSheet = res.get< SpriteSheet >( spriteSheetName );
+
+    auto matDef = res.get< MaterialDefinition >( materialName );
+    material = std::make_shared< Material >( matDef );
+    material->set( "tex0", spriteSheet->getTexture() );
+
+    for ( auto& s : sequences )
+    {
+        for ( auto& f : s.frameDefinitions )
+        {
+            s.frames.emplace_back( f.makeFrame( *spriteSheet, directions ) );
+        }
+    }
 }
 
-void Animation::setMaterialName(const String& n)
+void Animation::setName( const String& n )
 {
-	materialName = n;
+    name = n;
 }
 
-void Animation::setSpriteSheetName(const String& n)
+void Animation::setMaterialName( const String& n )
 {
-	spriteSheetName = n;
+    materialName = n;
 }
 
-void Animation::addSequence(const AnimationSequence& sequence)
+void Animation::setSpriteSheetName( const String& n )
 {
-	sequences.push_back(sequence);
+    spriteSheetName = n;
 }
 
-void Animation::addDirection(const AnimationDirection& direction)
+void Animation::addSequence( const AnimationSequence& sequence )
 {
-	directions.push_back(direction);
+    sequences.push_back( sequence );
 }
 
-const AnimationSequence& Animation::getSequence(const String& seqName) const
+void Animation::addDirection( const AnimationDirection& direction )
 {
-	for (auto& seq: sequences) {
-		if (seq.name == seqName) {
-			return seq;
-		}
-	}
-	return sequences.at(0);
+    directions.push_back( direction );
 }
 
-const AnimationDirection& Animation::getDirection(const String& dirName) const
+const AnimationSequence& Animation::getSequence( const String& seqName ) const
 {
-	Expects(directions.size() > 0);
-
-	for (auto& dir : directions) {
-		if (dir.name == dirName) {
-			return dir;
-		}
-	}
-	return directions[0];
+    for ( auto& seq : sequences )
+    {
+        if ( seq.name == seqName )
+        {
+            return seq;
+        }
+    }
+    return sequences.at( 0 );
 }
 
-const AnimationDirection& Animation::getDirection(int id) const
+const AnimationDirection& Animation::getDirection( const String& dirName ) const
 {
-	Expects(id >= 0);
-	Expects(directions.size() > 0);
+    Expects( directions.size() > 0 );
 
-	if (id < int(directions.size())) {
-		return directions[id];
-	} else {
-		return directions[0];
-	}
+    for ( auto& dir : directions )
+    {
+        if ( dir.name == dirName )
+        {
+            return dir;
+        }
+    }
+    return directions[ 0 ];
 }
 
-std::vector<String> Animation::getSequenceNames() const
+const AnimationDirection& Animation::getDirection( int id ) const
 {
-	std::vector<String> result;
-	for (auto& s: sequences) {
-		result.emplace_back(s.getName());
-	}
-	return result;
+    Expects( id >= 0 );
+    Expects( directions.size() > 0 );
+
+    if ( id < int( directions.size() ) )
+    {
+        return directions[ id ];
+    }
+    else
+    {
+        return directions[ 0 ];
+    }
 }
 
-std::vector<String> Animation::getDirectionNames() const
+std::vector< String > Animation::getSequenceNames() const
 {
-	std::vector<String> result;
-	for (auto& d: directions) {
-		result.emplace_back(d.getName());
-	}
-	return result;
+    std::vector< String > result;
+    for ( auto& s : sequences )
+    {
+        result.emplace_back( s.getName() );
+    }
+    return result;
+}
+
+std::vector< String > Animation::getDirectionNames() const
+{
+    std::vector< String > result;
+    for ( auto& d : directions )
+    {
+        result.emplace_back( d.getName() );
+    }
+    return result;
 }
 
 Vector2i Animation::getPivot() const
 {
-	return sequences.at(0).getFrame(0).getSprite(0).origPivot;
+    return sequences.at( 0 ).getFrame( 0 ).getSprite( 0 ).origPivot;
 }
 
 Rect4i Animation::getBounds() const
 {
-	auto& sprite = sequences.at(0).getFrame(0).getSprite(0);
-	const auto size = Vector2i(sprite.size) + Vector2i(sprite.trimBorder.xy() + sprite.trimBorder.zw());
-	const auto pivot = sprite.origPivot;
-	return Rect4i(-pivot, -pivot + size);
+    auto& sprite = sequences.at( 0 ).getFrame( 0 ).getSprite( 0 );
+    const auto size = Vector2i( sprite.size ) + Vector2i( sprite.trimBorder.xy() + sprite.trimBorder.zw() );
+    const auto pivot = sprite.origPivot;
+    return Rect4i( -pivot, -pivot + size );
 }
 
-bool Animation::hasSequence(const String& seqName) const
+bool Animation::hasSequence( const String& seqName ) const
 {
-	for (auto& s: sequences) {
-		if (s.getName() == seqName) {
-			return true;
-		}
-	}
-	return false;
+    for ( auto& s : sequences )
+    {
+        if ( s.getName() == seqName )
+        {
+            return true;
+        }
+    }
+    return false;
 }
 
-void Animation::serialize(Serializer& s) const
+void Animation::serialize( Serializer& s ) const
 {
-	s << name;
-	s << spriteSheetName;
-	s << materialName;
-	s << sequences;
-	s << directions;
+    s << name;
+    s << spriteSheetName;
+    s << materialName;
+    s << sequences;
+    s << directions;
 }
 
-void Animation::deserialize(Deserializer& s)
+void Animation::deserialize( Deserializer& s )
 {
-	s >> name;
-	s >> spriteSheetName;
-	s >> materialName;
-	s >> sequences;
-	s >> directions;
+    s >> name;
+    s >> spriteSheetName;
+    s >> materialName;
+    s >> sequences;
+    s >> directions;
 }
